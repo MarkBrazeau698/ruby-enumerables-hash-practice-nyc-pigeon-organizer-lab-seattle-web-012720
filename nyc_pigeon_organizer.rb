4490ae464d9704c0fcb7bb49c18a9f71f4fd954a
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-12 20:17:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do | color_gender_lives, value |
     value.each do | attributes, all_names |
       all_names.each do | name |
+        if pigeon_list[name] = nil
     end
     pigeon_list[name][color_gender_lives].push(attributes.to_s)
     end
